e4f309e3ab1a3f8df7a875c1f4abbd4e4ac2c280
PetrIvChe <cherniavskiipetr@gmail.com>
2021-09-14 14:44:44 +0300

    End work

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        
+    int account = 100;
+    int replenish = 1999;
+    int bonus;{
+            if (replenish <= 1000) {
+                bonus = 0;
+            } else  {
+                bonus = replenish / 100;
+            }
+        }
+    int replenishmentaccount = account + replenish + bonus;
+        System.out.println( "На вашем счету " + replenishmentaccount + " рублей");
+        System.out.println( "Из них, Вам начислено "+ bonus + " бонусных рублей." );
+
     }
 
 }
