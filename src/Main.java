a27f1105eedd5af02dfc8851579df96b30fa91c8
PetrIvChe <cherniavskiipetr@gmail.com>
2021-09-18 15:02:51 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,19 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-    int account = 100;
-    int replenish = 1999;
-    int bonus;{
+        int account = 100;
+        int replenish = 1999;
+        int bonus;
+        {
             if (replenish <= 1000) {
                 bonus = 0;
-            } else  {
+            } else {
                 bonus = replenish / 100;
             }
         }
-    int replenishmentaccount = account + replenish + bonus;
-        System.out.println( "На вашем счету " + replenishmentaccount + " рублей");
-        System.out.println( "Из них, Вам начислено "+ bonus + " бонусных рублей." );
+        int replenishmentaccount = account + replenish + bonus;
+        System.out.println("На вашем счету " + replenishmentaccount + " рублей");
+        System.out.println("Из них, Вам начислено " + bonus + " бонусных рублей.");
 
     }
 
